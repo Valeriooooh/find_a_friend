253f4bfe9d45ba86702746f005519b37943e9792
Diogo Valerio <diogocsvalerio@protonmail.com>
2024-08-28 05:40:38 +0100

    goRouter not working

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
+import 'package:find_a_friend/routes/router_config.dart';
 import 'package:flutter/material.dart';
 import 'package:rinf/rinf.dart';
 import './messages/generated.dart';
-import 'package:find_a_friend/messages/basic.pb.dart';
 
 void main() async {
   await initializeRust(assignRustSignal);
@@ -14,78 +14,78 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Find-A-Friend',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Find-A-Friend'),
-      debugShowCheckedModeBanner: false,
-    );
+    return MaterialApp.router(
+        title: 'Find-A-Friend',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+          useMaterial3: true,
+        ),
+        debugShowCheckedModeBanner: false,
+        routeInformationParser: MyAppRouter().router.routeInformationParser,
+        routerDelegate: MyAppRouter().router.routerDelegate);
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({super.key, required this.title});
+//   final String title;
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
 
-class _MyHomePageState extends State<MyHomePage> {
-  Iterable<ListTile> _listTiles = [];
-  void _addNewItem() {
-    setState(() {
-      DocumentCreateRequest().sendSignalToRust();
-      var _ = StreamBuilder(
-          stream: DocumentCreateResponse.rustSignalStream, // GENERATED
-          builder: (context, snapshot) {
-            final rustSignal = snapshot.data;
-            if (rustSignal == null) {
-              return Text("Not created Yet");
-            }
-            return Text(rustSignal.message.ticket);
-          });
-    });
-  }
+// class _MyHomePageState extends State<MyHomePage> {
+//   Iterable<ListTile> _listTiles = [];
+//   void _addNewItem() {
+//     setState(() {
+//       DocumentCreateRequest().sendSignalToRust();
+//       var _ = StreamBuilder(
+//           stream: DocumentCreateResponse.rustSignalStream, // GENERATED
+//           builder: (context, snapshot) {
+//             final rustSignal = snapshot.data;
+//             if (rustSignal == null) {
+//               return Text("Not created Yet");
+//             }
+//             return Text(rustSignal.message.ticket);
+//           });
+//     });
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    DocumentListRequest().sendSignalToRust();
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-        actions: [IconButton(icon: const Icon(Icons.camera), onPressed: () {})],
-      ),
-      body: StreamBuilder(
-          stream: DocumentListResponse.rustSignalStream, // GENERATED
-          builder: (context, snapshot) {
-            final rustSignal = snapshot.data;
-            if (rustSignal == null) {
-              return ListView(children: []);
-            }
-            _listTiles = rustSignal.message.document
-                .map((e) => {
-                      ListTile(
-                          leading:
-                              const Icon(color: Colors.lightBlue, Icons.radar),
-                          title: Text(e),
-                          onTap: () {}, //opens with goRouter to file
-                          onLongPress: () {} // should show a delete screen
-                          )
-                    })
-                .map((e) => e.toList())
-                .map((e) => e.first);
-            return ListView(
-              children: _listTiles.toList(),
-            );
-          }),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _addNewItem,
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     DocumentListRequest().sendSignalToRust();
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+//         title: Text(widget.title),
+//         actions: [IconButton(icon: const Icon(Icons.camera), onPressed: () {})],
+//       ),
+//       body: StreamBuilder(
+//           stream: DocumentListResponse.rustSignalStream, // GENERATED
+//           builder: (context, snapshot) {
+//             final rustSignal = snapshot.data;
+//             if (rustSignal == null) {
+//               return ListView(children: []);
+//             }
+//             _listTiles = rustSignal.message.document
+//                 .map((e) => {
+//                       ListTile(
+//                           leading:
+//                               const Icon(color: Colors.lightBlue, Icons.radar),
+//                           title: Text(e),
+//                           onTap: () {}, //opens with goRouter to file
+//                           onLongPress: () {} // should show a delete screen
+//                           )
+//                     })
+//                 .map((e) => e.toList())
+//                 .map((e) => e.first);
+//             return ListView(
+//               children: _listTiles.toList(),
+//             );
+//           }),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: _addNewItem,
+//       ),
+//     );
+//   }
+// }
