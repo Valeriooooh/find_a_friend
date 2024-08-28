50602e6cf22d454803b544f42312bbc3c9c98334
Diogo Valerio <diogocsvalerio@protonmail.com>
2024-08-28 04:54:55 +0100

    added list and camera icon

--- lib/main.dart
+++ lib/main.dart
@@ -17,21 +17,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Find-A-Friend',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
         useMaterial3: true,
       ),
@@ -43,16 +28,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -60,7 +35,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<StatelessWidget> _listTiles = [];
+  Iterable<ListTile> _listTiles = [];
   void _addNewItem() {
     setState(() {
       DocumentCreateRequest().sendSignalToRust();
@@ -73,64 +48,44 @@ class _MyHomePageState extends State<MyHomePage> {
             }
             return Text(rustSignal.message.ticket);
           });
-      //
-      _listTiles.clear();
     });
   }
 
   @override
   Widget build(BuildContext context) {
     DocumentListRequest().sendSignalToRust();
-    var a = StreamBuilder(
-      stream: DocumentListResponse.rustSignalStream, // GENERATED
-      builder: (context, snapshot) {
-        var titles;
-        final rustSignal = snapshot.data;
-        var temp;
-        if (rustSignal == null) {
-          temp = [];
-          _listTiles = temp;
-          return temp;
-        }
-        titles = rustSignal.message.document;
-        debugPrint("----> $titles");
-        for (var title in titles) {
-          temp.add(ListTile(
-            leading: const Icon(color: Colors.blue, Icons.radar),
-            title: Text("$title"),
-          ));
-        }
-        _listTiles = temp;
-        return temp;
-      },
-    );
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
+        actions: [IconButton(icon: const Icon(Icons.camera), onPressed: () {})],
       ),
-      body: ListView.builder(
-        itemCount: _listTiles.length,
-        itemBuilder: (context, index) {
-          return _listTiles[index];
-        },
-      ),
+      body: StreamBuilder(
+          stream: DocumentListResponse.rustSignalStream, // GENERATED
+          builder: (context, snapshot) {
+            final rustSignal = snapshot.data;
+            if (rustSignal == null) {
+              return ListView(children: []);
+            }
+            _listTiles = rustSignal.message.document
+                .map((e) => {
+                      ListTile(
+                          leading:
+                              const Icon(color: Colors.lightBlue, Icons.radar),
+                          title: Text(e),
+                          onTap: () {}, //opens with goRouter to file
+                          onLongPress: () {} // should show a delete screen
+                          )
+                    })
+                .map((e) => e.toList())
+                .map((e) => e.first);
+            return ListView(
+              children: _listTiles.toList(),
+            );
+          }),
       floatingActionButton: FloatingActionButton(
         onPressed: _addNewItem,
-        tooltip: "hello",
       ),
-      //       StreamBuilder(
-      //         stream: Incred.rustSignalStream, // GENERATED
-      //         builder: (context, snapshot) {
-      //           final rustSignal = snapshot.data;
-      //           if (rustSignal == null) {
-      //             return const Text('0');
-      //           }
-      //           final myTreasureOutput = rustSignal.message;
-      //           _counter = myTreasureOutput.number;
-      //           return Text('$_counter');
-      //         },
-      //      )
     );
   }
 }
--- native/hub/src/lib.rs
+++ native/hub/src/lib.rs
@@ -4,13 +4,22 @@
 mod common;
 mod messages;
 
-use futures::StreamExt;
-use iroh::{base::node_addr::AddrInfoOptions, blobs::store::fs::Store, client::docs::{self, ShareMode}, docs::AuthorId, net::relay::RelayMode, node::{Node, RpcStatus}};
 use common::Result;
-use messages::basic::{DocumentCreateRequest, DocumentCreateResponse, DocumentListRequest, DocumentListResponse};
+use futures::StreamExt;
+use iroh::{
+    base::node_addr::AddrInfoOptions,
+    blobs::store::fs::Store,
+    client::docs::{self, ShareMode},
+    docs::AuthorId,
+    net::relay::RelayMode,
+    node::{Node, RpcStatus},
+};
+use messages::basic::{
+    DocumentCreateRequest, DocumentCreateResponse, DocumentListRequest, DocumentListResponse,
+};
 // use messages;
 use tokio; // Comment this line to target the web.
-// use tokio_with_wasm::alias as tokio; // Uncomment this line to target the web.
+           // use tokio_with_wasm::alias as tokio; // Uncomment this line to target the web.
 
 rinf::write_interface!();
 
@@ -19,10 +28,13 @@ const IROH_DIR: &str = "/data/data/com.example.find_a_friend/files/";
 struct App;
 
 impl App {
-    async fn new() -> Result<(Node<Store>, AuthorId)>{
+    async fn new() -> Result<(Node<Store>, AuthorId)> {
         let mut node = Node::persistent(IROH_DIR).await.unwrap();
         let node = node.relay_mode(RelayMode::Default);
-        let node = node.enable_rpc_with_addr(iroh::node::DEFAULT_RPC_ADDR).await.unwrap();
+        let node = node
+            .enable_rpc_with_addr(iroh::node::DEFAULT_RPC_ADDR)
+            .await
+            .unwrap();
         let node = node.spawn().await.unwrap();
         let _author = node.authors().default().await.unwrap();
         Ok((node, _author))
@@ -36,13 +48,17 @@ impl App {
 // use `tokio::task::spawn_blocking`.
 async fn main() {
     let _ = RpcStatus::clear(IROH_DIR).await;
-    let Ok((mut node, author)) = App::new().await else{
+    let Ok((mut node, author)) = App::new().await else {
         {
             rinf::debug_print!("startup failed!");
             panic!("startup failed!");
         }
     };
-    rinf::debug_print!("startup succeded \n\n NodeId: {} \n\n AuthorId {}", node.node_id(), author.to_string());
+    rinf::debug_print!(
+        "startup succeded \n\n NodeId: {} \n\n AuthorId {}",
+        node.node_id(),
+        author.to_string()
+    );
     tokio::spawn(doc_list_get(node.clone()));
     tokio::spawn(doc_create(node.clone(), author.clone()));
 }
@@ -50,15 +66,17 @@ async fn main() {
 async fn doc_list_get(node: Node<Store>) -> Result<()> {
     let mut sig = DocumentListRequest::get_dart_signal_receiver()?;
     while let Some(dart_signal) = sig.recv().await {
-    let mut docs = vec![];
-    if let Ok(mut a) = node.docs().list().await{
-        while let Some(item) = a.next().await
-        {
-            docs.push(item.unwrap());
-        }
-    };
+        let mut docs = vec![];
+        if let Ok(mut a) = node.docs().list().await {
+            while let Some(item) = a.next().await {
+                docs.push(item.unwrap());
+            }
+        };
         rinf::debug_print!("{docs:?}");
-        DocumentListResponse{document: docs.iter().map(|(x,_)| x.to_string()).collect()}.send_signal_to_dart();
+        DocumentListResponse {
+            document: docs.iter().map(|(x, _)| x.to_string()).collect(),
+        }
+        .send_signal_to_dart();
     }
     Ok(())
 }
@@ -66,17 +84,21 @@ async fn doc_list_get(node: Node<Store>) -> Result<()> {
 async fn doc_create(node: Node<Store>, author: AuthorId) -> Result<()> {
     let mut sig = DocumentCreateRequest::get_dart_signal_receiver()?;
     while let Some(dart_signal) = sig.recv().await {
-        let Ok(mut new_doc) =
-            node.docs().create().await else {
-                DocumentCreateResponse{ticket: None}.send_signal_to_dart();
-                return Ok(());
-            };
-        let Ok(ticket) = new_doc.share(ShareMode::Write, AddrInfoOptions::Id).await else{
-            DocumentCreateResponse{ticket: None}.send_signal_to_dart();
+        let Ok(mut new_doc) = node.docs().create().await else {
+            DocumentCreateResponse { ticket: None }.send_signal_to_dart();
             return Ok(());
         };
-        let _ = new_doc.set_bytes(author, "share_ticket", ticket.to_string()).await;
-        DocumentCreateResponse{ticket: Some(ticket.to_string())}.send_signal_to_dart();
+        let Ok(ticket) = new_doc.share(ShareMode::Write, AddrInfoOptions::Id).await else {
+            DocumentCreateResponse { ticket: None }.send_signal_to_dart();
+            return Ok(());
+        };
+        let _ = new_doc
+            .set_bytes(author, "share_ticket", ticket.to_string())
+            .await;
+        DocumentCreateResponse {
+            ticket: Some(ticket.to_string()),
+        }
+        .send_signal_to_dart();
         rinf::debug_print!("{ticket:?}");
     }
     Ok(())
